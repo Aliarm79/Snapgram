97437ad3176e688f1b5e89d52f56d822f2941392
Aliarm79 <104495905+Aliarm79@users.noreply.github.com>
2024-01-09 18:15:41 +0330

    .

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -9,7 +9,7 @@ type PostCardProps = {
 
 const PostCard = ({ post }: PostCardProps) => {
   const { user } = useUserContext();
-  if (!post?.imageUrl) return;
+  console.log(post?.imageUrl);
   return (
     <div className="post-card">
       <div className="flex justify-between items-center">
