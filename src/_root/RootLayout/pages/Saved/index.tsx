f4c9e20019af09cf0192d962b3dd3cb7fdee2710
Aliarm79 <104495905+Aliarm79@users.noreply.github.com>
2024-02-07 22:58:48 +0330

    add saved post page

--- src/_root/RootLayout/pages/Saved/index.tsx
+++ src/_root/RootLayout/pages/Saved/index.tsx
@@ -1,8 +1,33 @@
+import GridPostList from "@/components/shared/GridPostList";
+import Loader from "@/components/shared/Loader";
+import { useGetCurrentUser } from "@/lib/react-query/queriesAndMutations";
+import { Models } from "appwrite";
 
 const Saved = () => {
+  const { data: user, isPending: userLoading } = useGetCurrentUser();
+  const posts = user?.save.map((item: Models.Document) => {
+    return item.post;
+  });
   return (
-    <div>Saved</div>
-  )
-}
+    <div className="flex flex-1   ">
+      <div className="home-container ">
+        <div className="flex justify-start items-center gap-3 w-full max-w-5xl">
+          <img src="/assets/icons/save.svg" alt="add" width={36} />
+          <h2 className="h3-bold md:h2-bold ">Saved Posts</h2>
+        </div>
 
-export default Saved
+        {userLoading ? (
+          <Loader />
+        ) : posts.length === 0 ? (
+          <div className="text-light-4 mt-10 text-center w-full">
+            No post found
+          </div>
+        ) : (
+          <GridPostList posts={posts} showStats={false} showUser={false} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Saved;
